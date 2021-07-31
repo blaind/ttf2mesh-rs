84b0003a3d7b5faf73d5bb84f8382a4e66d584e3
Mika Vatanen <blaind@blaind.net>
2021-07-31 13:03:18 +0300

    Prepare ttf2mesh-sys

---
+++ ttf2mesh-sys/README.md
@@ -0,0 +1,21 @@
+# ttf2mesh-sys
+
+This package provides FFI bindings to [ttf2mesh C-library](https://github.com/fetisov/ttf2mesh).
+
+This library should not be used directly. For higher-level Rust usage, see [ttf2mesh at crates.io](https://crates.io/crates/ttf2mesh)
+
+FFI bindings are provided for the supplied `ttf2mesh.c` self-contained code.
+
+The library is built at the build time, if you don't have compiler installed:
+
+```
+sudo apt-get install build-essential
+```
+
+(although a smaller subset might also be sufficient)
+
+Currently, a security patch from `ttf2mesh-fix.patch` is applied to the original library (see https://github.com/fetisov/ttf2mesh/pull/2). A patch command line utility is needed for building:
+
+```
+sudo apt-get install patch
+```
---
+++ ttf2mesh-sys/build.rs
@@ -0,0 +1,51 @@
+extern crate bindgen;
+
+use std::env;
+use std::path::PathBuf;
+
+fn main() {
+    println!("cargo:rerun-if-changed=wrapper.h");
+    println!("cargo:rerun-if-changed=ttf2mesh/ttf2mesh.c");
+    println!("cargo:rustc-link-lib=m");
+
+    let bindings = bindgen::Builder::default()
+        .header("wrapper.h")
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        .generate()
+        .expect("Unable to generate bindings");
+
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
+
+    let manifest_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    let patch_path = manifest_dir.join("ttf2mesh-fix.patch");
+    let patch_file = std::fs::File::open(patch_path).unwrap();
+
+    //// patching, see https://github.com/fetisov/ttf2mesh/pull/2
+    if std::process::Command::new("patch")
+        .arg("-v")
+        .status()
+        .is_err()
+    {
+        panic!("Can not apply ttf2mesh patch. Please make sure that you have `patch` command installed.")
+    }
+
+    std::process::Command::new("patch")
+        .current_dir(manifest_dir.join("ttf2mesh"))
+        .args("-r - --forward -p1".split(" "))
+        .stdin(patch_file)
+        .status()
+        .unwrap();
+    //// /patching
+
+    cc::Build::new()
+        .flag("-Wall")
+        .flag("-Wextra")
+        .flag("-pedantic")
+        .flag("-std=c99")
+        .flag("-D_POSIX_C_SOURCE=199309L")
+        .file("ttf2mesh/ttf2mesh.c")
+        .compile("ttf2mesh");
+}
