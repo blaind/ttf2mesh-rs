d1c9204d82549f26ec93c7c71a83de83a0e43a73
Mika Vatanen <blaind@blaind.net>
2023-11-19 14:13:39 +0200

    Upgrade bindgen to 0.69

--- ttf2mesh-sys/build.rs
+++ ttf2mesh-sys/build.rs
@@ -19,7 +19,7 @@ fn main() {
 
     let bindings = bindgen::Builder::default()
         .header("wrapper.h")
-        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
         .generate()
         .expect("Unable to generate bindings");
 
